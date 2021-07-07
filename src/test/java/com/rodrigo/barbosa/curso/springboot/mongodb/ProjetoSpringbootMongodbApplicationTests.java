0fbca93144d4754f59add3836669dced4567650c
rodrigobarbosa1406 <rodrigobarbosanb@hotmail.com>
2021-07-07 20:46:23 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/rodrigo/barbosa/curso/springboot/mongodb/ProjetoSpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.barbosa.curso.springboot.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
